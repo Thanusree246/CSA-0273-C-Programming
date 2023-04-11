94e697f8aea4d5e0abd3eb2911559fc2969548d4
Thanusree246 <130457481+Thanusree246@users.noreply.github.com>
2023-04-12 05:29:56 +0530

    Add files via upload

---
+++ d1 ass3.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int vowels = 0;
+    int i;
+
+    // Input string from user
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    // Count the number of vowels in the string
+    for (i = 0; i < strlen(str); i++) {
+        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' ||
+            str[i] == 'o' || str[i] == 'u' || str[i] == 'A' ||
+            str[i] == 'E' || str[i] == 'I' || str[i] == 'O' ||
+            str[i] == 'U') {
+            vowels++;
+        }
+    }
+
+    printf("Number of vowels in the string: %d\n", vowels);
+
+    return 0;
+}
